66547717cac6f0af04c74d1317f6cd5d97e27474
Kyle Shipley <kyleashipley@gmail.com>
2018-06-03 20:04:23 -0400

    Fix typo in dependency injection

--- FormBuilder.API/Startup.cs
+++ FormBuilder.API/Startup.cs
@@ -38,7 +38,7 @@ namespace FormBuilder.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, , DbInitializer dbInitializer)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DbInitializer dbInitializer)
         {
             if (env.IsDevelopment())
             {
